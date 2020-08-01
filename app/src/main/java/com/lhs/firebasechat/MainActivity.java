0bae76861cf084c50d4b9dbc2715e7bc6c3dd57d
lhs7091 <lhs7091@naver.com>
2020-08-01 17:49:54 +0900

    cloud messaging

--- app/src/main/java/com/lhs/firebasechat/MainActivity.java
+++ app/src/main/java/com/lhs/firebasechat/MainActivity.java
@@ -2,23 +2,45 @@ package com.lhs.firebasechat;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.net.PasswordAuthentication;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import tgio.rncryptor.RNCryptorNative;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder> mFirebaseAdapter;
+
+    public static final String MESSAGES_CHILD = "message";
+    public static final String PASSWORD = "1";
+
+    private DatabaseReference mFirebaseDatabaseReference;
+    private EditText mMessageEditText;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
@@ -49,6 +71,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mMessageEditText = findViewById(R.id.message_edit);
+
+        findViewById(R.id.send_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChatMessage chatMessage = new ChatMessage(encrypt(mMessageEditText.getText().toString()), mUsername, mPhotoUrl, null);
+                // insert firebase
+                mFirebaseDatabaseReference.child(MESSAGES_CHILD)
+                        .push()
+                        .setValue(chatMessage);
+                mMessageEditText.setText("");
+            }
+        });
+
         mMessageRecyclerView = findViewById(R.id.message_recycler_view);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -63,6 +100,47 @@ public class MainActivity extends AppCompatActivity {
                 mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
             }
         }
+
+        Query query = mFirebaseDatabaseReference.child(MESSAGES_CHILD);
+        FirebaseRecyclerOptions<ChatMessage> options = new FirebaseRecyclerOptions.Builder<ChatMessage>()
+                .setQuery(query, ChatMessage.class)
+                .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<ChatMessage, MessageViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MessageViewHolder holder, int position, @NonNull ChatMessage model) {
+                holder.messageTextView.setText(decrypt(model.getText()));
+                holder.nameTextView.setText(model.getName());
+                if(model.getPhotoUrl() == null){
+                    holder.photoImageView.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_baseline_account_circle_24));
+                }else{
+                    Glide.with(MainActivity.this).load(model.getPhotoUrl()).into(holder.photoImageView);
+                }
+            }
+
+            @NonNull
+            @Override
+            public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+                return new MessageViewHolder(view);
+            }
+        };
+
+        mMessageRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFirebaseAdapter.stopListening();
     }
 
     @Override
@@ -80,10 +158,22 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(this, SignInActivity.class);
                 intent.putExtra("signOut", true);
                 startActivity(intent);
+                finish();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
 
     }
+
+    public String encrypt(String raw){
+        RNCryptorNative rncryptor = new RNCryptorNative();
+        return new String(rncryptor.encrypt(raw, PASSWORD));
+    }
+
+    public String decrypt(String encrypt){
+        RNCryptorNative rncryptor = new RNCryptorNative();
+        return rncryptor.decrypt(encrypt, PASSWORD);
+    }
+
 }
